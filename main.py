ae7b2a16319d259bafb540512fde403a6f44f1e2
MTC <ghbihuy123@gmail.com>
2023-11-04 16:42:50 +0700

    data ingestion notebook added

--- main.py
+++ main.py
@@ -1,3 +1,13 @@
+from textSummerizer.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
 from textSummerizer.logging import logger
 
-logger.info("Welcome to our custom logging")
+STAGE_NAME = 'Data Ingestion stage'
+
+try:
+    logger.info(f">>>>>>> stage {STAGE_NAME} started <<<<<<<<<")
+    data_ingestion = DataIngestionTrainingPipeline()
+    data_ingestion.main()
+    logger.info(f">>>>>>> stage {STAGE_NAME} completed <<<<<<<<<")
+except Exception as e:
+    logger.exception(e)
+    raise e
